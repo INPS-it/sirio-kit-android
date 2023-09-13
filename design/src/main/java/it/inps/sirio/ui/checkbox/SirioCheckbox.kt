4fdfb70dcaf89e852b3148495ff64095a88122c1
Andrea Giancarli <andrea.giancarli@progettopa.it>
2023-09-13 15:16:43 +0200

    Lora font 125th INPS logo Minor fix

---
+++ design/src/main/java/it/inps/sirio/ui/checkbox/SirioCheckbox.kt
@@ -0,0 +1,52 @@
+//
+// SirioCheckbox.kt
+//
+// SPDX-FileCopyrightText: 2022 Istituto Nazionale Previdenza Sociale
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+package it.inps.sirio.ui.checkbox
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import it.inps.sirio.theme.SirioTheme
+
+/**
+ * The Sirio checkbox
+ *
+ * @param text The string on the checkbox right
+ * @param checked Whether the checkbox is checked
+ * @param enabled Whether the checkbox is enabled
+ * @param onCheckedChange The callback when the checkbox state change
+ */
+@Composable
+fun SirioCheckbox(
+    text: String? = null,
+    checked: Boolean,
+    enabled: Boolean = true,
+    onCheckedChange: (Boolean) -> Unit,
+) {
+    SirioCheckboxCommon(
+        text = text,
+        checked = checked,
+        enabled = enabled,
+        onCheckedChange = onCheckedChange,
+    )
+}
+
+@Preview
+@Composable
+private fun CheckboxPreview() {
+    SirioTheme {
+        Column {
+            SirioCheckbox(
+                text = "Title",
+                checked = false,
+                onCheckedChange = {},
+                enabled = true
+            )
+        }
+    }
+}
