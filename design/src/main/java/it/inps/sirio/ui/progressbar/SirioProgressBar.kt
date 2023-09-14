b56b71b4c6baf7c3081c1195cf25c2a96a51d96e
Andrea Giancarli <andrea.giancarli@progettopa.it>
2023-09-14 14:31:55 +0200

    Updated copyright

--- design/src/main/java/it/inps/sirio/ui/progressbar/SirioProgressBar.kt
+++ design/src/main/java/it/inps/sirio/ui/progressbar/SirioProgressBar.kt
@@ -1,7 +1,7 @@
 //
 // SirioProgressBar.kt
 //
-// SPDX-FileCopyrightText: 2022 Istituto Nazionale Previdenza Sociale
+// SPDX-FileCopyrightText: 2023 Istituto Nazionale Previdenza Sociale
 //
 // SPDX-License-Identifier: BSD-3-Clause
 //
--- sample/src/main/java/it/inps/design/notification/NotificationInlineActivity.kt
+++ sample/src/main/java/it/inps/design/notification/NotificationInlineActivity.kt
@@ -1,7 +1,7 @@
 //
 // NotificationInlineActivity.kt
 //
-// SPDX-FileCopyrightText: 2022 Istituto Nazionale Previdenza Sociale
+// SPDX-FileCopyrightText: 2023 Istituto Nazionale Previdenza Sociale
 //
 // SPDX-License-Identifier: BSD-3-Clause
 //
